1dffd5c1d01bb04dc1942108f87ce9334497ba71
Tony Trinh <tony19@gmail.com>
2021-08-09 03:34:32 -0500

    demo awaiting promise in jest

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>Hello</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class MyComponent extends Vue {
+  public result = 0;
+
+  public ready = false;
+
+public myMethod(promise: Promise<void>) {
+  return promise
+    .then(() => {
+      this.result = 1;
+    })
+    .catch(() => {
+      this.result = 2;
+    })
+    .finally(() => {
+      this.ready = true;
+    });
+}
+}
+</script>
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,35 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import MyComponent from '@/components/MyComponent.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+describe('MyComponent.vue', () => {
+  // variable 'component' contains 'wrapper.vm', after the component has been shallow mounted, using vue-test-utils
+  it('should set the result to 1 and be ready', async () => {
+    const component = shallowMount(MyComponent).vm as any
+
+    // Given
+    const promise = Promise.resolve();
+
+    // When
+    await component.myMethod(promise);
+    await expect(promise).resolves.toBeUndefined();
+
+    // Then
+    expect(component.result).toBe(1);
+    expect(component.ready).toBe(true);
+  });
+
+  it('should set the result to 2 and be ready', async () => {
+    const component = shallowMount(MyComponent).vm as any
+
+    // Given
+    const promise = Promise.reject();
+
+    // When
+    await component.myMethod(promise);
+    await expect(promise).rejects.toBeUndefined();
+
+    // Then
+    expect(component.result).toBe(2);
+    expect(component.ready).toBe(true);
+  });
 })
